72aba4f653d2c97755c704a341f8448a747e89a1
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-09-10 08:05:12 +0530

    ..

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -1,68 +1,68 @@
-import pdf from "./assets/tahavirdi_1.6year_SoftwareEngineer.pdf"
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import pdf from "./assets/tahavirdi_2years_SoftwareEngineer.pdf";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const header = {
-  homepage: 'https://github.com/tahavirdiwala/portfolio',
-  title: 'JS.',
-}
+  homepage: "https://github.com/tahavirdiwala/portfolio",
+  title: "JS.",
+};
 
 const about = {
-  name: 'Taha Virdiwala',
-  role: 'JavaScript Developer',
+  name: "Taha Virdiwala",
+  role: "JavaScript Developer",
   description:
-    'I like building to build amazingapps, and learning new programming languages',
+    "I like building to build amazingapps, and learning new programming languages",
   resume: pdf,
   social: {
-    linkedin: 'https://www.linkedin.com/in/taha-virdiwala-830909221/',
+    linkedin: "https://www.linkedin.com/in/taha-virdiwala-830909221/",
     linkedinIcon: LinkedInIcon,
-    github: 'https://github.com/tahavirdiwala',
+    github: "https://github.com/tahavirdiwala",
     icon: GitHubIcon,
   },
-}
+};
 
 const projects = [
   {
-    name: 'Cyro WebApp',
+    name: "Cyro WebApp",
     description:
-      'Static fast webapp with some nice animations with the help of framer motion',
-    stack: ['React', 'Framer motion', 'Css'],
-    livePreview: 'https://tahacyro.netlify.app/',
+      "Static fast webapp with some nice animations with the help of framer motion",
+    stack: ["React", "Framer motion", "Css"],
+    livePreview: "https://tahacyro.netlify.app/",
   },
   {
-    name: 'Todo App',
-    description: 'Todo App with crud functionalty',
-    stack: ['React, Redux'],
-    livePreview: 'https://tahatodoapp.netlify.app/',
+    name: "Todo App",
+    description: "Todo App with crud functionalty",
+    stack: ["React, Redux"],
+    livePreview: "https://tahatodoapp.netlify.app/",
   },
   {
-    name: 'Calculator App',
-    description: 'Calculator web app',
-    stack: ['React', 'Css'],
-    livePreview: 'https://tahacalci.netlify.app/',
+    name: "Calculator App",
+    description: "Calculator web app",
+    stack: ["React", "Css"],
+    livePreview: "https://tahacalci.netlify.app/",
   },
-]
+];
 
 const skills = [
-  'Rust',
-  'React',
-  'Redux',
-  'React Query',
-  'TypeScript',
-  'Nivo Chart',
-  'Nodejs',
-  'ExpressJs',
-  'Mysql',
-  'Mongo DB',
-  'JavaScript',
-  'Git',
-  'GitLab',
-  'HTML',
-  'CSS',
-]
+  "Rust",
+  "React",
+  "Redux",
+  "React Query",
+  "TypeScript",
+  "Nivo Chart",
+  "Nodejs",
+  "ExpressJs",
+  "Mysql",
+  "Mongo DB",
+  "JavaScript",
+  "Git",
+  "GitLab",
+  "HTML",
+  "CSS",
+];
 
 const contact = {
-  email: 'tahavirdiwala97@gmail.com',
-}
+  email: "tahavirdiwala97@gmail.com",
+};
 
-export { header, about, projects, skills, contact }
+export { header, about, projects, skills, contact };

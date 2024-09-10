149b0452746096d655c4efbe148ea101f1fada1c
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-09-10 08:08:09 +0530

    pdf done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,35 +1,34 @@
-import { about } from '../../portfolio'
-import './About.css'
-
+import { about } from "../../portfolio";
+import "./About.css";
 
 const About = () => {
-  const { name, role, description, resume, social } = about
+  const { name, role, description, resume, social } = about;
 
   const onDownload = () => {
     const pdfUrl = resume;
     const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = "tahavirdi_1.6year_SoftwareEngineer.pdf"; 
+    link.download = "tahavirdi_2years_SoftwareEngineer.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  }
+  };
 
   return (
-    <div className='about center'>
+    <div className="about center">
       {name && (
         <h1>
-          Hi, I am <span className='about__name'>{name}.</span>
+          Hi, I am <span className="about__name">{name}.</span>
         </h1>
       )}
 
-      {role && <h2 className='about__role'>A {role}.</h2>}
-      <p className='about__desc'>{description && description}</p>
+      {role && <h2 className="about__role">A {role}.</h2>}
+      <p className="about__desc">{description && description}</p>
 
-      <div className='about__contact center'>
+      <div className="about__contact center">
         {resume && (
-          <button className='btn btn--outline' onClick={onDownload}>
-             Resume
+          <button className="btn btn--outline" onClick={onDownload}>
+            Resume
           </button>
         )}
 
@@ -38,8 +37,8 @@ const About = () => {
             {social.github && (
               <a
                 href={social.github}
-                aria-label='github'
-                className='link link--icon'
+                aria-label="github"
+                className="link link--icon"
               >
                 <social.icon />
               </a>
@@ -48,17 +47,17 @@ const About = () => {
             {social.linkedin && (
               <a
                 href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
+                aria-label="linkedin"
+                className="link link--icon"
               >
-              <social.linkedinIcon />
+                <social.linkedinIcon />
               </a>
             )}
           </>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
